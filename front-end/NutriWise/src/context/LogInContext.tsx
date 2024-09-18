652709025520775a4cdcad240427e6a58f84326f
rodlima <rodlima@brasilseg.com.br>
2024-09-18 16:04:29 -0300

    finalizado tela de login

---
+++ front-end/NutriWise/src/context/LogInContext.tsx
@@ -0,0 +1,77 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface LogInProviderProps{
+    children: ReactNode
+}
+
+interface UserInterface{
+    name: string; 
+    birthdate: string;
+    gender: string;
+    weight: number;
+    height: number;
+    email: string; 
+    password: string; 
+    question1:string; 
+    question2: string;
+    question3: string;
+}
+
+export interface LogInInterface{
+    email: string;
+    password: string
+}
+
+interface MessageInterface{
+    loginValidated: boolean | any;
+    message: string;
+    type: 'success' | 'error';
+}
+
+interface ILogInContextData{
+    validated: MessageInterface;
+    userData: UserInterface;
+    getUserData: ({email,password}: LogInInterface) => void;
+}
+
+const LogInContext = createContext({} as ILogInContextData);
+
+function LogInProvider({children}: LogInProviderProps){
+    const [userData, setUserData] = useState<UserInterface>({} as UserInterface)
+    const [validated, setValidated] = useState<MessageInterface>({} as MessageInterface)
+
+
+    function getUserData({email,password}: LogInInterface){
+        
+        //1. Pega todos os usuario no localStorage
+        const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+        //2. Procura no array de usuarios se a senha e password digitados contem no array usuarios
+        const validatedUser: UserInterface = users.find((user: UserInterface) => {
+            return user.email === email && user.password === password;
+        });
+
+        //3. Se tiver um usuario com a mesma senha e login, salva as informaçoes na variavel
+        if(validatedUser){
+            setUserData(validatedUser);
+            setValidated({loginValidated: true, message: "Login sucesso", type:"success"})
+            
+        }else{
+            setValidated({loginValidated: false, message: "Senha ou e-mail incorretos", type:"error"})
+        }
+        
+    }
+
+    return(
+        <LogInContext.Provider value={{userData, getUserData,validated}}>
+            {children}
+        </LogInContext.Provider>
+    )
+}
+
+function useLogIn(){
+    const context = useContext(LogInContext);
+    return context
+}
+
+export { LogInProvider, useLogIn};
--- front-end/NutriWise/src/routes/routes.tsx
+++ front-end/NutriWise/src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes as Switch, Route } from 'react-router-dom'
 import Main from '../Pages/Main/Index';
 import SignUp from '../Pages/SignUp/Index';
+import LogIn from '../Pages/LogIn/Index';
 
 const Routes = () => {
   return (
     <Switch>
         <Route path="/" element={<Main />} />
         <Route path="/singup" element={<SignUp />} />
+        <Route path="/logIn" element={<LogIn />} />
     </Switch>
   );
 }
